779d0e9968c727cfcbd9a88221b22fa1c3ef5bf0
Dario Dourado <dariodourado@gmail.com>
2023-05-18 12:45:34 +0100

    MaisPoPBtn
    Mais Popular Btn

--- OnlineSchool/src/feactures/CoursesList/components/CoursesList.jsx
+++ OnlineSchool/src/feactures/CoursesList/components/CoursesList.jsx
@@ -9,20 +9,8 @@ import { getAllCourses } from '../CourseListSlice'
 
 export default function CoursesList() {
 
-  const [cursos, setCursos] = useState(coursesInfo)
+  // const [cursos, setCursos] = useState(coursesInfo)
 
-  // let dispatch = useDispatch();
-  // dispatch(getAllCourses(coursesInfo))
-
-  // useEffect(() => {
-  //   fetch('../../data.json')
-  //     .then(resp => resp.json())
-  //     .then(dataFromJson => dispatch(getAllCourses(dataFromJson)))
-  //     .catch(err => console.log('Ocurreu um erro'))
-  // }, []);
-
-
-  
   return (
     <section className='container mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl'>
       <section className='coursesList flex flex-col  justify-between items-center'>
